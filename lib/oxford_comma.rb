27bf92ce2540794e6970196f0cfc2d6167291eef
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 06:01:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-  
+  def oxford_comma(fruits)
+    it fruits.length == 2 
+    "#{fruits[0]} and #{fruits[1]}"
+    elsif fruits.length > 2 
+    last_fruit = fruits.pop
+    fruits.join(", ") << ", and #{last_fruit}"
+  else fruits.le\ == 1 
+    "#{fruits[0]"
+end
+  end 
 end
