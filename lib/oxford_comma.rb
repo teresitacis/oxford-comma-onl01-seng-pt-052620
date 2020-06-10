ed8e7e59463f62a733e770527d7a09c0d40181e9
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 06:11:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  def oxford_comma(fruits)
-    it fruits.length == 2 
-    "#{fruits[0]} && #{fruits[1]}"
-    elsif fruits.length > 2 
-    last_fruit = fruits.pop
-    fruits.join(", ") << ", && #{last_fruit}"
-  else fruits.le\ == 1 
-    "#{fruits[0]"
-end 
+  case array.length 
+  when 1 
+    "#{array[0]}"
+    when 2 
+      array[0..1].join(", ") << ", and #{array[-1]}"
+    end
+  
+end
